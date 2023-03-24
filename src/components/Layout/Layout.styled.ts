1446936c29222014ae1bcdaf8b48460835f76ff8
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-03-25 00:31:26 +0200

    add layout

---
+++ src/components/Layout/Layout.styled.ts
@@ -0,0 +1,73 @@
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const Header = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+
+  background-color: var(--color-bg-transparent);
+  z-index: 10;
+`;
+
+export const Container = styled.div`
+  box-sizing: border-box;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1440px;
+  padding-left: 80px;
+  padding-right: 80px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 13px 0px 14px;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
+`;
+
+export const StyledNavLink = styled(NavLink)`
+  padding-top: 8px;
+  padding-bottom: 8px;
+  color: var(--color-font-light);
+
+  font-family: "Lato", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.1875;
+  text-transform: uppercase;
+  border-bottom: 1px solid transparent;
+
+  transition: border-bottom var(--anim);
+
+  :hover,
+  :focus {
+    border-bottom: 1px solid var(--color-font-light);
+  }
+
+  &.active {
+    border-bottom: 1px solid var(--color-font-light);
+  }
+`;
+
+export const BtnGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const StyledLink = styled(Link)`
+  width: 53px;
+  height: 53px;
+  background-color: var(--color-btn-secondary);
+`;
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,65 @@
+import { Link, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
+import logo from "../../images/logo/logo.png";
+import { ReactComponent as Heart } from "../../images/svg/heart.svg";
+import {
+  BtnGroup,
+  Container,
+  Header,
+  List,
+  StyledNavLink,
+  Wrapper,
+} from "./Layout.styled";
+import { Button } from "../Button/Button";
+import { IconNavLink } from "../IconNavLink/IconNavLink";
+
+const Layout: React.FC = () => {
+  const location = useLocation();
+  return (
+    <>
+      <Header>
+        <Container>
+          <Wrapper>
+            <Link to="/">
+              <img src={logo} alt="logo" width="229" />
+            </Link>
+            <nav>
+              <List>
+                <li>
+                  <StyledNavLink to="/">Home</StyledNavLink>
+                </li>
+                <li>
+                  <StyledNavLink to="tours">Tours</StyledNavLink>
+                </li>
+                <li>
+                  <StyledNavLink to="about">About</StyledNavLink>
+                </li>
+                <li>
+                  <StyledNavLink to="help">Help</StyledNavLink>
+                </li>
+              </List>
+            </nav>
+            <BtnGroup>
+              <IconNavLink>
+                <Heart
+                  style={{
+                    fill: location.pathname.includes("favourites")
+                      ? "#fff"
+                      : "#1e1e1e",
+                  }}
+                />
+              </IconNavLink>
+              <Button text="Sign In" />
+            </BtnGroup>
+          </Wrapper>
+        </Container>
+      </Header>
+      <main>
+        <Outlet></Outlet>
+      </main>
+    </>
+  );
+};
+
+export default Layout;
