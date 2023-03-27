95a57788d356a751118a4901d16ccca3021d6a7b
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-03-27 21:49:15 +0300

    add lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import React from "react";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+
 import Layout from "./components/Layout/Layout";
-import FavouritesPage from "./pages/FavouritesPage/FvouritesPage";
-import HomePage from "./pages/HomePage/HomePage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const FavouritesPage = lazy(
+  () => import("./pages/FavouritesPage/FvouritesPage")
+);
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="favourites" element={<FavouritesPage />} />
-      </Route>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="favourites" element={<FavouritesPage />} />
+        </Route>
 
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 };
 
